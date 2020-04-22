5d494db71d5fa683fe7aee8dfdfcde786ec1be1c
julienemo <juliette.nada@gmail.com>
2020-04-22 22:41:46 +0200

    [add] subject 1 in O(n)

--- exercices.js
+++ exercices.js
@@ -13,6 +13,30 @@ const arrayHasSumON2 = (array, sum) => {
   }
   return result;
 };
+// O(n) UGLY!
+const arrayHasSumON = (array, sum) => {
+  // see whether there are duplicates
+  let freqObj = {};
+  for (i = 0; i < array.length; i++) {
+    if (freqObj[array[i]] === undefined) {
+      freqObj[array[i]] = 1;
+    } else {
+      freqObj[array[i]] += 1;
+    }
+  }
+  // if duplicates, and if happens to be half of sum, bingo
+  if (freqObj[sum / 2] >= 2) {
+    return true;
+  }
+  // else
+  let keys = Object.keys(freqObj).map((x) => +x);
+  for (i = 0; i < array.length; i++) {
+    if (keys.includes(sum - array[i]) && array[i] * 2 !== sum) {
+      return true;
+    }
+  }
+  return false;
+};
 
 // one single iteration
 const arrayHasSumSingleIte = (array, sum) => {
@@ -42,7 +66,22 @@ const numberWithViewON2 = (array) => {
   }
   return result;
 };
-// one single iteration
+
+// O(n) : solution kindly shared by Florian S
+const buildingWithViewFlo = (array) => {
+  let count = 1;
+  let s = array.length - 1;
+  let pseudo_min = array[s];
+  for (let i = s; i > 0; i--) {
+    if (array[i - 1] > pseudo_min) {
+      count += 1;
+      pseudo_min = array[i - 1];
+    }
+  }
+  return count;
+};
+
+// one single iteration (you said one single loop but not O(n)!!)
 const numberWithViewSingleIte = (array) => {
   let s = array.length - 1;
   let result = 1;
@@ -63,31 +102,27 @@ const numberWithViewSingleIte = (array) => {
 // #############################################################
 // results
 
-/* console.log(arrayHasSumON2([1, 2, 3, 4, 5], 6));
+console.log(arrayHasSumON2([1, 2, 3, 4, 5], 6));
+console.log(arrayHasSumON([1, 2, 3, 4, 5], 6));
 console.log(arrayHasSumSingleIte([1, 2, 3, 4, 5], 6));
 
 console.log(arrayHasSumON2([1, 2, 3, 4, 5], 10));
+console.log(arrayHasSumON([1, 2, 3, 4, 5], 10));
 console.log(arrayHasSumSingleIte([1, 2, 3, 4, 5], 10));
 
 console.log(arrayHasSumON2([10, 15, 3, 7], 17));
+console.log(arrayHasSumON([10, 15, 3, 7], 17));
 console.log(arrayHasSumSingleIte([10, 15, 3, 7], 17));
 
 console.log(arrayHasSumON2([1, 8, 10, 21], 19));
-console.log(arrayHasSumSingleIte([1, 8, 10, 21], 19)); */
+console.log(arrayHasSumON([1, 8, 10, 21], 19));
+console.log(arrayHasSumSingleIte([1, 8, 10, 21], 19));
+
+console.log(arrayHasSumON2([1, 2, 3, 5, 5], 10));
+console.log(arrayHasSumON([1, 2, 3, 5, 5], 10));
+console.log(arrayHasSumSingleIte([1, 2, 3, 5, 5], 10));
 
 //#################
-const buildingWithViewFlo = (array) => {
-  let count = 1;
-  let s = array.length - 1;
-  let pseudo_min = array[s];
-  for (let i = s; i > 0; i--) {
-    if (array[i - 1] > pseudo_min) {
-      count += 1;
-      pseudo_min = array[i - 1];
-    }
-  }
-  return count;
-};
 
 console.log(numberWithViewON2([1, 2, 3, 4, 5]));
 console.log(numberWithViewSingleIte([1, 2, 3, 4, 5]));
