41a03d8218546b9cfd12839de653815b0f69e070
julienemo <juliette.nada@gmail.com>
2020-04-22 20:05:23 +0200

    [fix] ruby tests altered ref issue

--- sorting.rb
+++ sorting.rb
@@ -1,11 +1,3 @@
-# TODO: test whether variables pass by ref
-# ex : array = somearray
-# copy = array
-# modify copy
-# test whether array is also altered
-# look up how to make a new array from an existing array as in js
-
-
 def bubble_sort(array)
   bubble_count = 0
   copy = array
@@ -21,10 +13,104 @@ def bubble_sort(array)
     end
     return array unless continue
   end
-  puts "Bubble sort => #{array} with #{bubble_count} comparaisons"
   array
 end
 
-def select_sort(array)
-  
+def selection_sort(array)
+  select_count = 0
+  copy = array[0..-1]
+  s = copy.length - 1
+  (0..s).each do |i|
+    max = i
+    (i + 1..s).each do |j|
+      select_count += 1
+      max = j if array[j] > array[max]
+    end
+    array[max], array[j] = array[j], array[max] if max != i
+  end
+  array
 end
+
+def insertion_sort(array)
+  insert_count = 0
+  s = array.length - 1
+  (0..s).each do |i|
+    x = array[i]
+    j = i
+    while j.positive? && array[i - 1] < x
+      insert_count = 0
+      array[j] = array[j - 1]
+      j -= 1
+    end
+    array[j] = x
+  end
+  array
+end
+
+def partition(array, left, right)
+  p = array[((left + right) / 2).ceil]
+  while left <= right
+    left += 1 while array[left] > p
+    right -= 1 while array[right] < p
+    if left <= right
+      array[left], array[right] = array[right], array[left]
+      left += 1
+      right -= 1
+    end
+  end
+  left
+end
+
+def quick_sort(array, left = 0, right = array.length - 1)
+  if array.length > 1
+    index = partition(array, left, right)
+    quick_sort(array, left, index - 1) if left < index - 1
+    quick_sort(array, index, right) if index < right
+  end
+  array
+end
+
+def merge_sort(array, a)
+  a += 1
+  puts a
+  p array
+  puts ''
+  if array.length > 1
+    mid = ((array[0] + array[-1]) / 2).round(0)
+    left = array[0..mid]
+    right = array[(mid + 1)..-1]
+    merge_sort(left, a)
+    merge_sort(right, a)
+    i, j, k = 0, 0, 0
+    while (i < left.length) && (j < right.length)
+      if left[i] > right[j]
+        array[k] = left[i]
+        i += 1
+      else
+        array[k] = right[j]
+        j += 1
+      end
+      k += 1
+    end
+    while i < left.length
+      array[k] = left[i]
+      i += 1
+      k += 1
+    end
+    while j < right.length
+      array[k] = right[j]
+      j += 1
+      k += 1
+    end
+  end
+  array
+end
+
+# in ruby, array is passed by reference
+# or every object is passed by reference
+# ex:
+# array = [1, 2, 3, 4, 5]
+# copy = array
+# copy[3] = 'a'
+# puts copy // [1, 2, 'a', 4, 5]
+# puts array // [1, 2, 'a', 4, 5]
--- spec/sorting_spec.rb
+++ spec/sorting_spec.rb
@@ -1,44 +1,51 @@
 require_relative '../sorting'
 
 array1 = [1, -2, 3, -5, 4, 9, -6, 7, 8]
-array2 = [48, -2, 6, 12, 0, -4]
-array3 = [8763, -133, 246, -714, 42, 42, 2339, -42, 1506, 651, -321]
+array2 = [6, 48, -2, 12, 0, -4]
+array3 = [246, 8763, -133, -714, 42, 42, 2339, -42, 1506, 651, -321]
 array4 = [5, 3, 2, 1]
 
 describe 'bubble sort' do
   it 'sorts array in descending order' do
-    expect(bubble_sort(array1)).to eq(array1.sort.reverse)
-    expect(bubble_sort(array2)).to eq(array2.sort.reverse)
-    expect(bubble_sort(array3)).to eq(array3.sort.reverse)
-    expect(bubble_sort(array4)).to eq(array4.sort.reverse)
+    expect(bubble_sort(array1[0..-1])).to eq(array1[0..-1].sort.reverse)
+    expect(bubble_sort(array2[0..-1])).to eq(array2[0..-1].sort.reverse)
+    expect(bubble_sort(array3[0..-1])).to eq(array3[0..-1].sort.reverse)
+    expect(bubble_sort(array4[0..-1])).to eq(array4[0..-1].sort.reverse)
   end
 end
 
-# need to delete commented part to run test
-=begin describe 'selection sort' do
+describe 'selection sort' do
   it 'sorts array in descending order' do
-    expect(selection_sort(array1)).to eq(array1.sort.reverse)
-    expect(selection_sort(array2)).to eq(array2.sort.reverse)
-    expect(selection_sort(array3)).to eq(array3.sort.reverse)
-    expect(selection_sort(array4)).to eq(array4.sort.reverse)
+    expect(selection_sort(array1[0..-1])).to eq(array1[0..-1].sort.reverse)
+    expect(selection_sort(array2[0..-1])).to eq(array2[0..-1].sort.reverse)
+    expect(selection_sort(array3[0..-1])).to eq(array3[0..-1].sort.reverse)
+    expect(selection_sort(array4[0..-1])).to eq(array4[0..-1].sort.reverse)
   end
 end
 
 describe 'insertion sort' do
   it 'sorts array in descending order' do
-    expect(insertion_sort(array1)).to eq(array1.sort.reverse)
-    expect(insertion_sort(array2)).to eq(array2.sort.reverse)
-    expect(insertion_sort(array3)).to eq(array3.sort.reverse)
-    expect(insertion_sort(array4)).to eq(array4.sort.reverse)
+    expect(insertion_sort(array1[0..-1])).to eq(array1[0..-1].sort.reverse)
+    expect(insertion_sort(array2[0..-1])).to eq(array2[0..-1].sort.reverse)
+    expect(insertion_sort(array3[0..-1])).to eq(array3[0..-1].sort.reverse)
+    expect(insertion_sort(array4[0..-1])).to eq(array4[0..-1].sort.reverse)
   end
 end
 
 describe 'quick sort' do
   it 'sorts array in descending order' do
-    expect(quick_sort(array1)).to eq(array1.sort.reverse)
-    expect(quick_sort(array2)).to eq(array2.sort.reverse)
-    expect(quick_sort(array3)).to eq(array3.sort.reverse)
-    expect(quick_sort(array4)).to eq(array4.sort.reverse)
+    expect(quick_sort(array1[0..-1])).to eq(array1[0..-1].sort.reverse)
+    expect(quick_sort(array2[0..-1])).to eq(array2[0..-1].sort.reverse)
+    expect(quick_sort(array3[0..-1])).to eq(array3[0..-1].sort.reverse)
+    expect(quick_sort(array4[0..-1])).to eq(array4[0..-1].sort.reverse)
   end
 end
- =end
+
+describe 'merge sort' do
+  it 'sorts array in descending order' do
+    #expect(merge_sort(array1[0..-1], 0)).to eq(array1[0..-1].sort.reverse)
+    #expect(merge_sort(array2)).to eq(array2.sort.reverse)
+    #expect(merge_sort(array3)).to eq(array3.sort.reverse)
+    #expect(merge_sort(array4)).to eq(array4.sort.reverse)
+  end
+end
